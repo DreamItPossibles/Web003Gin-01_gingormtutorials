c4b71a46e44be230ce510b8ffa9d633f0d72a93f
liujinshun <liujinshun@163.com>
2026-03-09 18:19:15 +0800

    写项目的第一步

---
+++ go_code/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+type Config struct {
+	App struct {
+		Name string
+		Port string
+	}
+	Database struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		name     string
+	}
+}
+
+var AppConfig *Config
