912432aa4db90e3e7b1389a6fcec408b31c2637f
liujinshun <liujinshun@163.com>
2026-03-10 13:06:16 +0800

    gin初体验

--- go_code/main.go
+++ go_code/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"go_code/config"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.InitConfig()
 	fmt.Println(config.AppConfig.App.Port)
+
+	r := gin.Default()
+	r.GET("ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(config.AppConfig.App.Port)
 }
