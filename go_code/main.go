585db913c589120c2a609ba95dc5645a26b3384d
liujinshun <liujinshun@163.com>
2026-03-10 13:36:46 +0800

    接口路由配置初体验

--- go_code/main.go
+++ go_code/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"go_code/config"
-
-	"github.com/gin-gonic/gin"
+	"go_code/router"
 )
 
 func main() {
 	config.InitConfig()
-	fmt.Println(config.AppConfig.App.Port)
 
-	r := gin.Default()
-	r.GET("ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r := router.SetupRouter()
 
-	r.Run(config.AppConfig.App.Port)
+	port := config.AppConfig.App.Port
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	r.Run(port)
 }
